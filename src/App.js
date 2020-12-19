475dc04a5b060044f10b95955a540a8b1f1186cc
dokdo <jcs191072@gmail.com>
2020-12-20 04:19:30 +0900

    feat: Implement submit button logic by passing Props up search

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
     users: [],
     loading: false,
   };
+
   async componentDidMount() {
     this.setState({ loading: true });
     const response = await axios.get(
@@ -19,11 +20,25 @@ class App extends Component {
 
     this.setState({ users: response.data, loading: false });
   }
+
+  // Search Github Users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${text}
+      &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: response.data.items, loading: false });
+  };
+
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <Search />
+        <Search searchUsers={this.searchUsers} />
         <div className='container'>
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
